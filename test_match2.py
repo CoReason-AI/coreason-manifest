14a7bfb409c9980e141d57d3854f3f1874374313
Gowtham Rao MD PhD <gowtham.rao@coreason.ai>
2026-03-03 10:05:22 -0500

    remove lingering files

--- test_match2.py
+++
@@ -1,8 +0,0 @@
-from coreason_manifest.core.state.persistence import JSONPatchOperation, PatchOp
-
-p = JSONPatchOperation(op=PatchOp.ADD, path="/a", value=5)
-match p.model_dump(exclude_unset=True):
-    case {"op": "add", "path": path, "value": val}:
-        print("ADD", path, val)
-    case _:
-        print("MISS")
