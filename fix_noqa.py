ed788010f493055931a3af856836dd683be94328
Gowtham Rao MD PhD <gowtham.rao@coreason.ai>
2026-03-15 08:05:23 -0400

    remove temporary files that snuck in

--- fix_noqa.py
+++
@@ -1,9 +0,0 @@
-with open("src/coreason_manifest/spec/ontology.py") as f:
-    lines = f.readlines()
-
-for i, line in enumerate(lines):
-    if len(line) > 120 and "noqa" not in line:
-        lines[i] = line.rstrip("\n") + "  # noqa: E501\n"
-
-with open("src/coreason_manifest/spec/ontology.py", "w") as f:
-    f.writelines(lines)
