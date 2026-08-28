683994a877457e46bba51c3c05c317fbc1cf3166
gowthamrao <13936600+gowthamrao@users.noreply.github.com>
2026-08-28 23:27:37 +0000

    Fix RUF036 rule violation by moving None to the end of JsonPrimitiveState union

--- src/coreason_manifest/spec/ontology.py
+++ src/coreason_manifest/spec/ontology.py
@@ -752,10 +752,10 @@ type JsonPrimitiveState = (
     | int
     | float
     | bool
-    | None
     | list["JsonPrimitiveState"]
     | dict[str, "JsonPrimitiveState"]
     | EpistemicProxyState[Any]
+    | None
 )
 
 
